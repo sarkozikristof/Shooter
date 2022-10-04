b07289281fde27a1826d2d0116ebb563fcd1d259
sarkozikristof <krisi.sarkozi@gmail.com>
2022-10-04 17:10:36 +0200

    Foundation

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import pygame as pg
+from settings import *
+
+
 class Game:
     def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RESOLUTION)
+        self.clock = pg.time.Clock()
+        self.new_game()
+
+    def new_game(self):
         pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
+    
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+
+    def run(self):
+        while True:
+            self.draw()
+            self.update()
+            self.check_events()
+
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
